8a9d95662cbf6f29d1bffe8985fcf29a3577b9ad
unknown <sreechaitra725@gmail.com>
2026-08-30 17:27:48 +0530

    Updated init.sql

--- database/init.sql
+++ database/init.sql
@@ -15,7 +15,8 @@ VALUES
 	('456789012345','Pavani Praveen','2007-05-25','FEMALE',''),
 	('498714770290','Poornima V','2007-07-20','FEMALE',''),
 	('935349568480','Tanuja Thakur','2007-02-08','FEMALE','9107076376956206'),
-	('893831116226','Darakhshan Parween Zeeshan Shaikh','1997-02-25','FEMALE','9187647016408093');
+	('893831116226','Darakhshan Parween Zeeshan Shaikh','1997-02-25','FEMALE','9187647016408093'),
+	('687006240742','Sukumar Karuppiah','1978-05-01','MALE','9181803855623212');
 
 CREATE TABLE pan (
 pan_num VARCHAR(12) PRIMARY KEY,
@@ -60,7 +61,7 @@ VALUES
     'IND',
     'KAUSHAL',
     'SUKHDEEP',
-    'INDIAN',
+    'IND',
     'F',
     '1991-10-20',
     'GALI KALAN',
@@ -74,7 +75,7 @@ VALUES
     'IND',
     'KALUVA',
     'SANTHOSHI',
-    'INDIAN',
+    'IND',
     'F',
     '1993-07-09',
     'HYDERABAD, TELANGANA',
@@ -88,7 +89,7 @@ VALUES
     'IND',
     'SANDHU',
     'GAGANDEEP SINGH',
-    'INDIAN',
+    'IND',
     'M',
     '1997-02-01',
     'GANGOHAR, PUNJAB',
@@ -102,7 +103,7 @@ VALUES
     'IND',
     'KAHLON',
     'GURVIR SINGH',
-    'INDIAN',
+    'IND',
     'M',
     '1990-07-19',
     'MACHHIWARA',
@@ -116,12 +117,41 @@ VALUES
     'IND',
 	'',
     'MAQDOOMA FATHIMA',
-    'INDIAN',
+    'IND',
     'F',
     '1981-06-23',
     'CHENNAI, TAMIL NADU',
     'BENGALURU',
     '2017-12-15',
     '2027-12-14'
+),
+(
+	'H91379927',
+	'P',
+	'IND',
+	'ALAM',
+	'MAQSOOD',
+	'IND',
+	'M',
+	'1973-08-14',
+	'MUZAFFARPUR BIHAR',
+	'DELHI',
+	'2010-02-18',
+	'2020-02-17'
+),
+(
+	'J7335300',
+	'P',
+	'IND',
+	'',
+	'JASPREET KAUR',
+	'IND',
+	'F',
+	'1994-09-24',
+	'RAIKOT, PUNJAB',
+	'CHANDIGARH',
+	'2011-05-24',
+	'2021-05-23'
+	
 );
 	
